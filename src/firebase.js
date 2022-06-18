bfc5bb790d528ccbf5f1199297ed0912bcbff988
Vishal Singh <90970004+mankotia412vishal@users.noreply.github.com>
2022-06-18 22:11:55 +0530

    VippyDrive1

--- src/firebase.js
+++ src/firebase.js
@@ -1,26 +1,26 @@
 import firebase from "firebase"
 
 // ProblemAuth
-// const firebaseConfig = {
-//   apiKey: "AIzaSyC9dnd1KviDPEcvg3zDx7Gknc1KlSvdjcU",
-//   authDomain: "problemauth-da73f.firebaseapp.com",
-//   projectId: "problemauth-da73f",
-//   storageBucket: "problemauth-da73f.appspot.com",
-//   messagingSenderId: "399517848095",
-//   appId: "1:399517848095:web:f33675fec6e5363352fb2a"
-// };
+const firebaseConfig = {
+  apiKey: "AIzaSyC9dnd1KviDPEcvg3zDx7Gknc1KlSvdjcU",
+  authDomain: "problemauth-da73f.firebaseapp.com",
+  projectId: "problemauth-da73f",
+  storageBucket: "problemauth-da73f.appspot.com",
+  messagingSenderId: "399517848095",
+  appId: "1:399517848095:web:f33675fec6e5363352fb2a"
+};
 
 
 // VishalProject
 
-const firebaseConfig = {
-  apiKey: "AIzaSyDjV4aDFBK-_eQ8NKEa1gO0TOuOijLlQ80",
-  authDomain: "vishalproject-47d7a.firebaseapp.com",
-  projectId: "vishalproject-47d7a",
-  storageBucket: "vishalproject-47d7a.appspot.com",
-  messagingSenderId: "678515285625",
-  appId: "1:678515285625:web:e68e0b0ccd2fa230e3fea8"
-};
+// const firebaseConfig = {
+//   apiKey: "AIzaSyDjV4aDFBK-_eQ8NKEa1gO0TOuOijLlQ80",
+//   authDomain: "vishalproject-47d7a.firebaseapp.com",
+//   projectId: "vishalproject-47d7a",
+//   storageBucket: "vishalproject-47d7a.appspot.com",
+//   messagingSenderId: "678515285625",
+//   appId: "1:678515285625:web:e68e0b0ccd2fa230e3fea8"
+// };
 const firebaseApp=firebase.initializeApp(firebaseConfig);
 
 const db=firebaseApp.firestore();
